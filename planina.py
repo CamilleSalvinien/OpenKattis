19c08e58c548c4382ecfc6ec4801b595cd687a0a
CamilleSalvinien <70179401+CamilleSalvinien@users.noreply.github.com>
2021-01-06 15:14:42 -0500

    Add files via upload

---
+++ planina.py
@@ -0,0 +1,33 @@
+x = int(input())
+if x == 1:
+    print(3**2)
+elif x == 2:
+    print(5**2)
+elif x == 3:
+    print(9**2)
+elif x == 4:
+    print(17**2)
+elif x == 5:
+    print(33**2)
+elif x == 6:
+    print(65**2)
+elif x == 7:
+    print(129**2)
+elif x == 8:
+    print(257**2)
+elif x == 9:
+    print(513**2)
+elif x == 10:
+    print(1025**2)
+elif x == 11:
+    print(2049**2)
+elif x == 12:
+    print(4097**2)
+elif x == 13:
+    print(8193**2)
+elif x == 14:
+    print(16385**2)
+elif x == 15:
+    print(32769**2)
+else:
+    pass
