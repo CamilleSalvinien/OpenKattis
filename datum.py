ad0d6d69530c9b868ac8340dfe9da1aabc5d118e
Camille Salvinien <csalvinien@usf.edu>
2021-02-01 19:12:26 -0500

    Update datum.py

--- datum.py
+++ datum.py
@@ -3,7 +3,6 @@ import datetime
 days = ["Monday", "Tuesday", "Wednesday", "Thursday",
     "Friday", "Saturday", "Sunday"]
 
-
 d, m = map(int, input().split())
 
 weekdays = datetime.date(2009,m,d).weekday()
