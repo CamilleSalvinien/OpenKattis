c3f4ca3c70cf0d1add7afbd2f0cbd5cf16c25ccb
Camille Salvinien <csalvinien@usf.edu>
2021-05-09 12:25:25 -0400

    Update aaah.py

--- aaah.py
+++ aaah.py
@@ -1,7 +1,11 @@
-x = input()
-y = input()
+# Take the input of what Jon Marius can say and what the Doctor wants to hear
+jonMarius = input()
+doctorHear = input()
 
-if len(x) >= len(y):
+# If Jon Marius can hold the "Aaah.." longer or equal to the doctor,
+# he will go to the doctor
+if len(jonMarius) >= len(doctorHear):
     print ("go")
+# Otherwise, he will not
 else:
     print("no")
