5d699d9e91a84cb9f6fc1a4fdfc1fc45df1b3f9b
Camille Salvinien <csalvinien@usf.edu>
2021-05-09 14:09:49 -0400

    Update batterup.py

--- batterup.py
+++ batterup.py
@@ -1,18 +1,17 @@
-x = int(input())
-y = [int(y) for y in input().split()]  
-list = []
-count = 0
+# Take the number of bats and bat scores as in put
+numOfBats = int(input())
+bats = [int(y) for y in input().split()]  
 
-for i in range(x):
-  for x in (y):
-    if x>(-1):
-      count +=1
-      list.append(x)
-      i -=1
-    else:
-      i -=1
+# Create two counts, one to count the sum of all bats (not walks)
+# and the count of bats (not walks)
+countSum = 0
+countBat = 0
 
-print((sum(list))/count)     
+# Increment the counters if the integer is greater or equal to 0
+for i in range (len(bats)):
+    if bats[i] >= 0:
+        countSum += int(bats[i])
+        countBat += 1
 
-list = []
-count = 0
+# Print the ratio of the sum of bats divided by the number of bats
+print(countSum/countBat) 
