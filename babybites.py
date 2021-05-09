cfd84a8829efe9e0399e5957a0463351133204d2
Camille Salvinien <csalvinien@usf.edu>
2021-05-09 13:54:54 -0400

    Update babybites.py

--- babybites.py
+++ babybites.py
@@ -1,21 +1,29 @@
-n = int(input())
-a = list(input().split())
+# Take the number of bites and what Arild says
+numOfBites = int(input())
+talk = list(input().split())
+# Create a list to track if the couting is correct
 numbers = []
 
-for i in range (len(a)):
-    if a[i].isnumeric() == True:
-        numbers.append(int(a[i]))
-    elif a[i] == "mumble":
+# Add the numbers, and add the index+1 if mumble
+for i in range (len(talk)):
+    if talk[i].isnumeric() == True:
+        numbers.append(int(talk[i]))
+    elif talk[i] == "mumble":
         numbers.append(i+1)
 
+# Create a counter
 count = 0
 
+# Check if the number is equal to index+1
+# Otherwise increment counter by 1
 for i in range (len(numbers)):
     if numbers[i] == (i+1):
         pass
     else:
         count+=1
 
+# If the counter is equal to 0 print out MAKES SENSE
+# Else print out SOMETHING IS FISHY
 if count == 0:
     print("makes sense")
 else:
