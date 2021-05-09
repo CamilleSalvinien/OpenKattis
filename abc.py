d65c7de52c97d5dec8123d8370b10a4103e563de
Camille Salvinien <csalvinien@usf.edu>
2021-05-09 12:30:59 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,15 +1,23 @@
-numbers = list(map(int, input().split()))
-numbers = sorted(numbers)
+# Take the input numbers and sort them
+numbers = sorted(list(map(int, input().split())))
 
+# Take the letters as input in a list
 letters = list(input())
+
+# Create a list to print out the output
 output = []
 
+# Loop for the numbers of letters given
 for i in range (len(letters)):
+    # If the letter is an A, append to the first place of the output list
     if letters[i] == "A":
         output.append(numbers[0])
+    # If the letter is a B, append to the second place of the output list
+    elif letters[i] == "B":
+        output.append(numbers[1])
+    # If the letter is a C, append to the third place of the output list
     elif letters[i] == "C":
         output.append(numbers[2])
-    else:
-        output.append(numbers[1])
 
-print(' '.join(str(p) for p in output))
+# Print out the output
+print(' '.join(str(j) for j in output))
