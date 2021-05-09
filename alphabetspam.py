8b26b23d2a9bbcce56099e532b65c677bd3822ee
Camille Salvinien <csalvinien@usf.edu>
2021-05-09 12:36:41 -0400

    Update alphabetspam.py

--- alphabetspam.py
+++ alphabetspam.py
@@ -1,11 +1,16 @@
+# Take the input of the sentence
 sentence = input()
 
+# Create four counters for the whitespace, lowercase,
+# uppercase and symbols
 whitespace = 0
 lowercase = 0
 uppercase = 0
 symbols = 0
 
+# Loop through each character of the sentence given
 for i in range (len(sentence)):
+    # Increase the counter for the relevant character
     if sentence[i] == "_":
         whitespace+=1
     elif sentence[i].isupper():
@@ -15,6 +20,8 @@ for i in range (len(sentence)):
     else:
         symbols+=1
 
+# For each counter print our the ratios of the counter to the
+# length of the count, to the 10**(-6)
 print("{:6f}".format(whitespace/len(sentence)))
 print("{:6f}".format(lowercase/len(sentence)))
 print("{:6f}".format(uppercase/len(sentence)))
